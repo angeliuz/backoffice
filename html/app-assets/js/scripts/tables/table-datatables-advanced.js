241c639c131c542866047ed3ed4e5e03d62297dd
Jorge A. Espinoza <jorge.espinoza@grupo-sm.com>
2021-09-11 20:51:19 -0300

    Gestor de recursos

--- html/app-assets/js/scripts/tables/table-datatables-advanced.js
+++ html/app-assets/js/scripts/tables/table-datatables-advanced.js
@@ -2,7 +2,7 @@
  * DataTables Advanced
  */
 
-'use strict';
+"use strict";
 
 // Advanced Search Functions Starts
 // --------------------------------------------------------------------
@@ -10,52 +10,66 @@
 // Filter column wise function
 function filterColumn(i, val) {
   if (i == 5) {
-    var startDate = $('.start_date').val(),
-      endDate = $('.end_date').val();
-    if (startDate !== '' && endDate !== '') {
+    var startDate = $(".start_date").val(),
+      endDate = $(".end_date").val();
+    if (startDate !== "" && endDate !== "") {
       filterByDate(i, startDate, endDate); // We call our filter function
     }
 
-    $('.dt-advanced-search').dataTable().fnDraw();
+    $(".dt-advanced-search").dataTable().fnDraw();
   } else {
-    $('.dt-advanced-search').DataTable().column(i).search(val, false, true).draw();
+    $(".dt-advanced-search")
+      .DataTable()
+      .column(i)
+      .search(val, false, true)
+      .draw();
   }
 }
 
 // Datepicker for advanced filter
-var separator = ' - ',
-  rangePickr = $('.flatpickr-range'),
-  dateFormat = 'MM/DD/YYYY';
+var separator = " - ",
+  rangePickr = $(".flatpickr-range"),
+  dateFormat = "MM/DD/YYYY";
 var options = {
   autoUpdateInput: false,
   autoApply: true,
   locale: {
     format: dateFormat,
-    separator: separator
+    separator: separator,
   },
-  opens: $('html').attr('data-textdirection') === 'rtl' ? 'left' : 'right'
+  opens: $("html").attr("data-textdirection") === "rtl" ? "left" : "right",
 };
 
 //
 if (rangePickr.length) {
   rangePickr.flatpickr({
-    mode: 'range',
-    dateFormat: 'm/d/Y',
+    mode: "range",
+    dateFormat: "m/d/Y",
     onClose: function (selectedDates, dateStr, instance) {
-      var startDate = '',
+      var startDate = "",
         endDate = new Date();
       if (selectedDates[0] != undefined) {
         startDate =
-          selectedDates[0].getMonth() + 1 + '/' + selectedDates[0].getDate() + '/' + selectedDates[0].getFullYear();
-        $('.start_date').val(startDate);
+          selectedDates[0].getMonth() +
+          1 +
+          "/" +
+          selectedDates[0].getDate() +
+          "/" +
+          selectedDates[0].getFullYear();
+        $(".start_date").val(startDate);
       }
       if (selectedDates[1] != undefined) {
         endDate =
-          selectedDates[1].getMonth() + 1 + '/' + selectedDates[1].getDate() + '/' + selectedDates[1].getFullYear();
-        $('.end_date').val(endDate);
+          selectedDates[1].getMonth() +
+          1 +
+          "/" +
+          selectedDates[1].getDate() +
+          "/" +
+          selectedDates[1].getFullYear();
+        $(".end_date").val(endDate);
       }
-      $(rangePickr).trigger('change').trigger('keyup');
-    }
+      $(rangePickr).trigger("change").trigger("keyup");
+    },
   });
 }
 
@@ -71,9 +85,9 @@ var filterByDate = function (column, startDate, endDate) {
     // If our date from the row is between the start and end
     if (start <= rowDate && rowDate <= end) {
       return true;
-    } else if (rowDate >= start && end === '' && start !== '') {
+    } else if (rowDate >= start && end === "" && start !== "") {
       return true;
-    } else if (rowDate <= end && start === '' && end !== '') {
+    } else if (rowDate <= end && start === "" && end !== "") {
       return true;
     } else {
       return false;
@@ -85,22 +99,26 @@ var filterByDate = function (column, startDate, endDate) {
 var normalizeDate = function (dateString) {
   var date = new Date(dateString);
   var normalized =
-    date.getFullYear() + '' + ('0' + (date.getMonth() + 1)).slice(-2) + '' + ('0' + date.getDate()).slice(-2);
+    date.getFullYear() +
+    "" +
+    ("0" + (date.getMonth() + 1)).slice(-2) +
+    "" +
+    ("0" + date.getDate()).slice(-2);
   return normalized;
 };
 // Advanced Search Functions Ends
 
 $(function () {
-  var isRtl = $('html').attr('data-textdirection') === 'rtl';
+  var isRtl = $("html").attr("data-textdirection") === "rtl";
 
-  var dt_ajax_table = $('.datatables-ajax'),
-    dt_filter_table = $('.dt-column-search'),
-    dt_adv_filter_table = $('.dt-advanced-search'),
-    dt_responsive_table = $('.dt-responsive'),
-    assetPath = '../../../app-assets/';
+  var dt_ajax_table = $(".datatables-ajax"),
+    dt_filter_table = $(".dt-column-search"),
+    dt_adv_filter_table = $(".dt-advanced-search"),
+    dt_responsive_table = $(".dt-responsive"),
+    assetPath = "app-assets/";
 
-  if ($('body').attr('data-framework') === 'laravel') {
-    assetPath = $('body').attr('data-asset-path');
+  if ($("body").attr("data-framework") === "laravel") {
+    assetPath = $("body").attr("data-asset-path");
   }
 
   // Ajax Sourced Server-side
@@ -110,14 +128,14 @@ $(function () {
     var dt_ajax = dt_ajax_table.dataTable({
       processing: true,
       dom: '<"d-flex justify-content-between align-items-center mx-0 row"<"col-sm-12 col-md-6"l><"col-sm-12 col-md-6"f>>t<"d-flex justify-content-between mx-0 row"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>',
-      ajax: assetPath + 'data/ajax.php',
+      ajax: assetPath + "data/ajax.php",
       language: {
         paginate: {
           // remove previous & next text from pagination
-          previous: '&nbsp;',
-          next: '&nbsp;'
-        }
-      }
+          previous: "&nbsp;",
+          next: "&nbsp;",
+        },
+      },
     });
   }
 
@@ -126,12 +144,18 @@ $(function () {
 
   if (dt_filter_table.length) {
     // Setup - add a text input to each footer cell
-    $('.dt-column-search thead tr').clone(true).appendTo('.dt-column-search thead');
-    $('.dt-column-search thead tr:eq(1) th').each(function (i) {
+    $(".dt-column-search thead tr")
+      .clone(true)
+      .appendTo(".dt-column-search thead");
+    $(".dt-column-search thead tr:eq(1) th").each(function (i) {
       var title = $(this).text();
-      $(this).html('<input type="text" class="form-control form-control-sm" placeholder="Search ' + title + '" />');
+      $(this).html(
+        '<input type="text" class="form-control form-control-sm" placeholder="Buscar ' +
+          title +
+          '" />'
+      );
 
-      $('input', this).on('keyup change', function () {
+      $("input", this).on("keyup change", function () {
         if (dt_filter.column(i).search() !== this.value) {
           dt_filter.column(i).search(this.value).draw();
         }
@@ -139,24 +163,75 @@ $(function () {
     });
 
     var dt_filter = dt_filter_table.DataTable({
-      ajax: assetPath + 'data/table-datatable.json',
+      ajax: assetPath + "data/datatable_recursos_clasificados.json",
       columns: [
-        { data: 'full_name' },
-        { data: 'email' },
-        { data: 'post' },
-        { data: 'city' },
-        { data: 'start_date' },
-        { data: 'salary' }
+        { data: "nombre" },
+        { data: "tipo_recurso" },
+        { data: "oa" },
+        { data: "eje" },
+        { data: "claves" },
+        { data: "descargable" },
       ],
+      columnDefs: [
+        {
+          // For Responsive
+          className: "control",
+          orderable: false,
+          targets: 0,
+        },
+        {
+          // For Checkboxes
+          targets: 2,
+          orderable: true,
+          responsivePriority: 3,
+          render: function (data, type, full, meta) {
+            const objetivos = function () {
+              let conca = full["oa"];
+              let oas = "";
+              let priorizacion;
+
+              for (var i = 0; i < conca.length; i++) {
+                if (conca[i]["priorizacion"] == "1") {
+                  console.log(conca[i]["priorizacion"]);
+                  priorizacion =
+                    '<ion-icon name="star" class="text-warning"></ion-icon>';
+                } else {
+                  if (conca[i]["priorizacion"] == "2") {
+                    priorizacion =
+                      '<ion-icon name="star-outline" class="text-warning"></ion-icon>';
+                  } else {
+                    priorizacion = "";
+                  }
+                }
+                oas +=
+                  '<div class="d-flex align-items-center"><div class="text-primary fw-500 mep-5">' +
+                  conca[i]["oa_code"] +
+                  "</div>" +
+                  priorizacion +
+                  "</div>";
+                if (conca.length - 1 === i) {
+                  return oas;
+                }
+              }
+            };
+            return objetivos;
+          },
+          checkboxes: {
+            selectAllRender:
+              '<div class="form-check"> <input class="form-check-input" type="checkbox" value="" id="checkboxSelectAll" /><label class="form-check-label" for="checkboxSelectAll"></label></div>',
+          },
+        },
+      ],
+
       dom: '<"d-flex justify-content-between align-items-center mx-0 row"<"col-sm-12 col-md-6"l><"col-sm-12 col-md-6"f>>t<"d-flex justify-content-between mx-0 row"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>',
       orderCellsTop: true,
       language: {
         paginate: {
           // remove previous & next text from pagination
-          previous: '&nbsp;',
-          next: '&nbsp;'
-        }
-      }
+          previous: "&nbsp;",
+          next: "&nbsp;",
+        },
+      },
     });
   }
 
@@ -166,23 +241,23 @@ $(function () {
   // Advanced Filter table
   if (dt_adv_filter_table.length) {
     var dt_adv_filter = dt_adv_filter_table.DataTable({
-      ajax: assetPath + 'data/table-datatable.json',
+      ajax: assetPath + "data/table-datatable.json",
       columns: [
-        { data: 'responsive_id' },
-        { data: 'full_name' },
-        { data: 'email' },
-        { data: 'post' },
-        { data: 'city' },
-        { data: 'start_date' },
-        { data: 'salary' }
+        { data: "responsive_id" },
+        { data: "full_name" },
+        { data: "email" },
+        { data: "post" },
+        { data: "city" },
+        { data: "start_date" },
+        { data: "salary" },
       ],
 
       columnDefs: [
         {
-          className: 'control',
+          className: "control",
           orderable: false,
-          targets: 0
-        }
+          targets: 0,
+        },
       ],
       dom: '<"d-flex justify-content-between align-items-center mx-0 row"<"col-sm-12 col-md-6"l><"col-sm-12 col-md-6"f>>t<"d-flex justify-content-between mx-0 row"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>',
       orderCellsTop: true,
@@ -191,28 +266,28 @@ $(function () {
           display: $.fn.dataTable.Responsive.display.modal({
             header: function (row) {
               var data = row.data();
-              return 'Details of ' + data['full_name'];
-            }
+              return "Details of " + data["full_name"];
+            },
           }),
-          type: 'column',
+          type: "column",
           renderer: $.fn.dataTable.Responsive.renderer.tableAll({
-            tableClass: 'table'
-          })
-        }
+            tableClass: "table",
+          }),
+        },
       },
       language: {
         paginate: {
           // remove previous & next text from pagination
-          previous: '&nbsp;',
-          next: '&nbsp;'
-        }
-      }
+          previous: "&nbsp;",
+          next: "&nbsp;",
+        },
+      },
     });
   }
 
   // on key up from input field
-  $('input.dt-input').on('keyup', function () {
-    filterColumn($(this).attr('data-column'), $(this).val());
+  $("input.dt-input").on("keyup", function () {
+    filterColumn($(this).attr("data-column"), $(this).val());
   });
 
   // Responsive Table
@@ -220,38 +295,38 @@ $(function () {
 
   if (dt_responsive_table.length) {
     var dt_responsive = dt_responsive_table.DataTable({
-      ajax: assetPath + 'data/table-datatable.json',
+      ajax: assetPath + "data/table-datatable.json",
       columns: [
-        { data: 'responsive_id' },
-        { data: 'full_name' },
-        { data: 'email' },
-        { data: 'post' },
-        { data: 'city' },
-        { data: 'start_date' },
-        { data: 'salary' },
-        { data: 'age' },
-        { data: 'experience' },
-        { data: 'status' }
+        { data: "responsive_id" },
+        { data: "full_name" },
+        { data: "email" },
+        { data: "post" },
+        { data: "city" },
+        { data: "start_date" },
+        { data: "salary" },
+        { data: "age" },
+        { data: "experience" },
+        { data: "status" },
       ],
       columnDefs: [
         {
-          className: 'control',
+          className: "control",
           orderable: false,
-          targets: 0
+          targets: 0,
         },
         {
           // Label
           targets: -1,
           render: function (data, type, full, meta) {
-            var $status_number = full['status'];
+            var $status_number = full["status"];
             var $status = {
-              1: { title: 'Current', class: 'badge-light-primary' },
-              2: { title: 'Professional', class: ' badge-light-success' },
-              3: { title: 'Rejected', class: ' badge-light-danger' },
-              4: { title: 'Resigned', class: ' badge-light-warning' },
-              5: { title: 'Applied', class: ' badge-light-info' }
+              1: { title: "Current", class: "badge-light-primary" },
+              2: { title: "Professional", class: " badge-light-success" },
+              3: { title: "Rejected", class: " badge-light-danger" },
+              4: { title: "Resigned", class: " badge-light-warning" },
+              5: { title: "Applied", class: " badge-light-info" },
             };
-            if (typeof $status[$status_number] === 'undefined') {
+            if (typeof $status[$status_number] === "undefined") {
               return data;
             }
             return (
@@ -259,10 +334,10 @@ $(function () {
               $status[$status_number].class +
               '">' +
               $status[$status_number].title +
-              '</span>'
+              "</span>"
             );
-          }
-        }
+          },
+        },
       ],
       dom: '<"d-flex justify-content-between align-items-center mx-0 row"<"col-sm-12 col-md-6"l><"col-sm-12 col-md-6"f>>t<"d-flex justify-content-between mx-0 row"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>',
       responsive: {
@@ -270,26 +345,28 @@ $(function () {
           display: $.fn.dataTable.Responsive.display.modal({
             header: function (row) {
               var data = row.data();
-              return 'Details of ' + data['full_name'];
-            }
+              return "Details of " + data["full_name"];
+            },
           }),
-          type: 'column',
+          type: "column",
           renderer: $.fn.dataTable.Responsive.renderer.tableAll({
-            tableClass: 'table'
-          })
-        }
+            tableClass: "table",
+          }),
+        },
       },
       language: {
         paginate: {
           // remove previous & next text from pagination
-          previous: '&nbsp;',
-          next: '&nbsp;'
-        }
-      }
+          previous: "&nbsp;",
+          next: "&nbsp;",
+        },
+      },
     });
   }
 
   // Filter form control to default size for all tables
-  $('.dataTables_filter .form-control').removeClass('form-control-sm');
-  $('.dataTables_length .form-select').removeClass('form-select-sm').removeClass('form-control-sm');
+  $(".dataTables_filter .form-control").removeClass("form-control-sm");
+  $(".dataTables_length .form-select")
+    .removeClass("form-select-sm")
+    .removeClass("form-control-sm");
 });
