0a19cbe07bc93814554738dc9e1f8f25568ed9c3
davidjimenezsm <david.jimenez@grupo-sm.com>
2021-11-23 10:26:56 -0300

    david

---
+++ html/app-assets/js/scripts/extensions/ext-component-sweet-alerts-david.js
@@ -0,0 +1,50 @@
+/*=========================================================================================
+	File Name: ext-component-sweet-alerts.js
+	Description: A beautiful replacement for javascript alerts
+	----------------------------------------------------------------------------------------
+	Item Name: Vuexy  - Vuejs, HTML & Laravel Admin Dashboard Template
+	Author: Pixinvent
+	Author URL: hhttp://www.themeforest.net/user/pixinvent
+==========================================================================================*/
+$(function () {
+  'use strict';
+
+  var confirmText = $('#confirm-text');
+
+  var assetPath = 'app-assets/';
+  if ($('body').attr('data-framework') === 'laravel') {
+    assetPath = $('body').attr('data-asset-path');
+  }
+
+  //--------------- Confirm Options ---------------
+
+  // Confirm Text
+  if (confirmText.length) {
+    confirmText.on('click', function () {
+      Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, delete it!',
+        customClass: {
+          confirmButton: 'btn btn-primary',
+          cancelButton: 'btn btn-outline-danger ms-1'
+        },
+        buttonsStyling: false
+      }).then(function (result) {
+        if (result.value) {
+          Swal.fire({
+            icon: 'success',
+            title: 'Deleted!',
+            text: 'Your file has been deleted.',
+            customClass: {
+              confirmButton: 'btn btn-success'
+            }
+          });
+        }
+      });
+    });
+  }
+
+});
