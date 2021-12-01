825e784f193427eb4c0888294a22e587103bbeed
Jorge A. Espinoza <jorge.espinoza@grupo-sm.com>
2021-12-01 13:10:10 -0300

    Jorge Tooltips

--- html/app-assets/js/scripts/tables/planificaciones_evaluaciones_lista_erik.js
+++ html/app-assets/js/scripts/tables/planificaciones_evaluaciones_lista_erik.js
@@ -10,6 +10,7 @@ $(function () {
   var obj_oa = "";
 
   var dt_basic_table = $(".dt-column-search");
+  var dt_indicadores = $(".dt-indicadores");
 
   var assetPath = "app-assets/";
 
@@ -116,22 +117,74 @@ $(function () {
       dt_basic_table.DataTable().responsive.recalc();
     });
 
-    // dt_basic_table.on("click", ".sweetAlert", function (e, dt, type, indexes) {
-    //   console.log("id: " + this.id);
-    //   Swal.fire({
-    //     title: "Are you sure?",
-    //     text: "You won't be able to revert this!",
-    //     icon: "warning",
-    //     showCancelButton: true,
-    //     confirmButtonColor: "#3085d6",
-    //     cancelButtonColor: "#d33",
-    //     confirmButtonText: "Yes, delete it!",
-    //   }).then((result) => {
-    //     if (result.isConfirmed) {
-    //       Swal.fire("Deleted!", "Your file has been deleted.", "success");
-    //     }
-    //   });
-    // });
+    $("div.head-label").html('<h6 class="mb-0">DataTable with Buttons</h6>');
+  }
+
+  if (dt_indicadores.length) {
+    dt_indicadores.DataTable({
+      processing: true,
+      api: true,
+      dom: '<"d-flex justify-content-between align-items-center mx-0 row"<"col-sm-12 col-md-6"l><"col-sm-12 col-md-6"f>>t<"d-flex justify-content-between mx-0 row"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>',
+      ajax: assetPath + "data/datatable_planificaciones_indicadores_lista.json",
+      destroy: true,
+      language: {
+        paginate: {
+          // remove previous & next text from pagination
+          previous: "&nbsp;",
+          next: "&nbsp;",
+          limit: 5,
+        },
+      },
+      columns: [
+        { data: "id", width: "80px" },
+        { data: "indicador", autoWidth: true },
+        { data: "nombre_objetivo", width: "190px" },
+      ],
+      columnDefs: [
+        {
+          targets: 0,
+        },
+        {
+          targets: 2,
+          data: "download_link",
+          render: function (data, type, row, meta) {
+            let html =
+              '<div class="d-flex justify-content-start flex-column>"' +
+              '<div class="text-start" ><div class="text-primary text-start fw-500" data-bs-html="true" data-bs-toggle="tooltip" title="<div class=\'text-start\'>' +
+              row.descripcion +
+              '</div>" id="' +
+              row.id +
+              '"  >' +
+              row.nombre_objetivo +
+              "</div>" +
+              '<div data-bs-toggle="tooltip" title="' +
+              row.nombre_priorizacion +
+              '">' +
+              (row.id_priorizacion == 1 ? '<ion-icon name="star" class="text-warning msp-5"></ion-icon></div>' : "") +
+              (row.id_priorizacion == 2 ? '<ion-icon name="star-outline" class="text-warning msp-5"></ion-icon></div>' : "") +
+              (row.id_priorizacion == 3 ? "" : "") +
+              "</div>" +
+              "</div>";
+            return html;
+          },
+        },
+      ],
+      language: {
+        url: "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json",
+      },
+      drawCallback: function () {
+        $(document).find('[data-bs-toggle="tooltip"]').tooltip();
+      },
+    });
+    //dt_basic_table.columns.adjust().draw();
+    dt_indicadores.DataTable().columns.adjust();
+    //dt_indicadores.DataTable().responsive.recalc();
+
+    $("#modal_indicadores").on("shown.bs.modal", function () {
+      dt_indicadores.DataTable().columns.adjust();
+      // dt_indicadores.DataTable().responsive.recalc();
+    });
+
     $("div.head-label").html('<h6 class="mb-0">DataTable with Buttons</h6>');
   }
 });
