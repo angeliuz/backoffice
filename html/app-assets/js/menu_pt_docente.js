645846191ad31434febf0e5edac48b4008755cd5
Jorge Espinoza <jorge.espinoza@grupo-sm.com>
2021-12-15 09:51:25 -0300

    Jorge Plataforma del docente

---
+++ html/app-assets/js/menu_pt_docente.js
@@ -0,0 +1,90 @@
+// CREADO POR JORGE ESPINOZA
+// NOVIEMBRE DE 2021
+// ERA PANDEMIC
+
+// initialization of go to
+// $.HSCore.components.HSGoTo.init(".js-go-to");
+// Example menu definititon:
+const menu = [
+  {
+    title: "Mis Cursos",
+    enlace: "#",
+    icon: "layers",
+  },
+  {
+    title: "Evaluaciones",
+    enlace: "#",
+    icon: "clipboard",
+  },
+  {
+    title: "Compartidos",
+    enlace: "gc_guias_lista.html",
+    icon: "book-open",
+  },
+  {
+    title: "Mensajes",
+    enlace: "#",
+    icon: "users",
+  },
+  {
+    title: "Ayuda",
+    enlace: "#",
+    icon: "home",
+  },
+];
+
+function populateMenu(menu) {
+  $("#main-menu-navigation").html("");
+
+  console.log(window.location.pathname);
+
+  let active = "";
+
+  let path = window.location.pathname.replace("/html/", "");
+  console.log(path);
+
+  for (let i = 0; i < menu.length; i++) {
+    let botonSolo = "";
+    if (menu[i].submenus) {
+      //console.log(menu[i].submenus);
+      botonSolo = '<li class="nav-item has-sub" id="menuParent' + i + '">';
+      botonSolo += '<a class="d-flex align-items-center" href="' + menu[i].enlace + '">';
+      botonSolo += '<i data-feather="' + menu[i].icon + '"></i>';
+      botonSolo += '<span class="menu-title text-truncate">' + menu[i].title + "</span>";
+      botonSolo += "</a>";
+
+      botonSolo += '<ul class="menu-content">';
+
+      for (let a = 0; a < menu[i].submenus.length; a++) {
+        // console.log(menu[i].submenus[a].title);
+        if (menu[i].submenus[a].enlace == path) {
+          console.log(menu[i].submenus[a].enlace + " == " + path);
+          active = "active";
+        } else {
+          active = "";
+        }
+
+        botonSolo += '<li class="' + active + '" id="element' + a + '">';
+        botonSolo += '<a class="d-flex align-items-center" href="' + menu[i].submenus[a].enlace + '">';
+        botonSolo += '<i data-feather="' + menu[i].submenus[a].icon + '"></i>';
+        botonSolo += '<span class="menu-item text-truncate">' + menu[i].submenus[a].title + "</span>";
+        botonSolo += "</a>";
+        botonSolo += "</li>";
+      }
+      botonSolo += "  </ul>";
+      botonSolo += "</li>";
+
+      $("#main-menu-navigation").append(botonSolo);
+    } else {
+      if (menu[i].enlace == path) {
+        console.log(menu[i].enlace + " == " + path);
+        active = "active";
+      } else {
+        active = "";
+      }
+      botonSolo = '<li class="nav-item ' + active + '">' + '<a class="d-flex align-items-center" href="' + menu[i].enlace + '">' + '<i data-feather="' + menu[i].icon + '"></i>' + '<span class="menu-title text-truncate">' + menu[i].title + "</span>";
+      $("#main-menu-navigation").append(botonSolo);
+    }
+  }
+}
+// Provide the DOM element where the menu should be inserted:
---
+++ html/app-assets/js/scripts/tables/plataforma_docente/table_cursos.js
@@ -0,0 +1,157 @@
+/**
+ * DataTables Basic
+ */
+
+ $(function () {
+    'use strict';
+  
+    var dt_basic_table = $('.dt-cursos')
+    var assetPath = '../app-assets/';
+
+    console.log(assetPath+"data/plataforma_docente/datatable_cursos.json")
+  
+    // DataTable with buttons
+    // --------------------------------------------------------------------
+  
+    if (dt_basic_table.length) {
+      var dt_basic = dt_basic_table.DataTable({
+        ajax: assetPath+'data/plataforma_docente/datatable_cursos.json',
+        processing: true,
+        api: true,
+        dom: '<"d-flex justify-content-between align-items-center mx-0 row"<"col-sm-12 col-md-6"l><"col-sm-12 col-md-6"f>>t<"d-flex justify-content-between mx-0 row"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>',
+        destroy: true,
+        language: {
+          paginate: {
+            // remove previous & next text from pagination
+            previous: "&nbsp;",
+            next: "&nbsp;",
+            limit: 5,
+          },
+        },
+        columns: [
+            { data: '' },
+            { data: 'comentarios' },
+            { data: 'estado' },
+            { data: '' }
+          ],
+          columnDefs: [
+            { 
+                targets:0 ,
+                render: function (data, type, full, meta) {
+                    
+                    var $name = full['area'];
+                    var $post = full['nivel'];
+                    
+                    // For Avatar badge
+                    var stateNum = full['id_area'];
+                    var states = ['', 'warning', 'danger', 'success', 'primary', 'info', 'secondary', 'dark'];
+                    var $state = states[stateNum],
+                    $name = full['area'],
+                    $initials = $name.match(/\b\w/g) || [];
+                    $initials = (($initials.shift() || '') + ($initials.pop() || '')).toUpperCase();
+                    var $output = '<span class="avatar-content">' + $initials + '</span>';
+                
+
+                    var colorClass = ' bg-light-' + $state;
+                    // Creates full output for row
+                    var $row_output =
+                      '<div class="d-flex justify-content-left align-items-center">' +
+                      '<div class="avatar ' +
+                      colorClass +
+                      ' me-1">' +
+                      $output +
+                      '</div>' +
+                      '<div class="d-flex flex-column">' +
+                      '<span class="emp_name text-truncate fw-bold">' +
+                      $name +
+                      '</span>' +
+                      '<small class="emp_post text-truncate text-muted">' +
+                      $post +
+                      '</small>' +
+                      '</div>' +
+                      '</div>';
+                    return $row_output;
+                },
+            },
+            {
+                targets: 1,
+                orderable: true,
+                className: 'text-center',
+                width: '150px',
+                render: function (data, type, full, meta) {
+                    var resp = '';
+                    if(full['comentarios']>0){
+                    var resp = '<div class="position-relative d-inline-block">'+
+                    feather.icons['mail'].toSvg({ class: 'font-medium-4' }) +
+                    '<span class="badge rounded-pill bg-danger badge-up">'+
+                    full['comentarios']+
+                    '</span>'+
+                    '</div>'
+                    }
+                    return resp;
+                }
+            },
+            {
+              // Label
+              targets: 2,
+              className: 'text-start',
+              width: '150px',
+              render: function (data, type, full, meta) {
+                var $status_number = full['estado'];
+                var $status = {
+                    1: { title: 'Validado', class: ' badge-light-success' },
+                    2: { title: 'Esperando validación', class: 'badge-light-warning' },
+                    3: { title: 'Corregir', class: ' badge-light-danger' },
+                };
+                if (typeof $status[$status_number] === 'undefined') {
+                  return data;
+                }
+                return (
+                  '<span class="badge rounded-pill ' +
+                  $status[$status_number].class +
+                  '">' +
+                  $status[$status_number].title +
+                  '</span>'
+                );
+              }
+            },
+            {
+              // Actions
+              targets: 3,
+              orderable: false,
+              className: 'text-center',
+              width: '80px',
+              render: function (data, type, full, meta) {
+                return (
+                  '<div class="d-inline-flex">' +
+                  '<a class="pe-1 dropdown-toggle hide-arrow text-primary" data-bs-toggle="dropdown">' +
+                  feather.icons['more-vertical'].toSvg({ class: 'font-small-4' }) +
+                  '</a>' +
+                  '<div class="dropdown-menu dropdown-menu-end">' +
+                  '<a href="javascript:;" class="dropdown-item">' +
+                  feather.icons['file-text'].toSvg({ class: 'me-50 font-small-4' }) +
+                  'Details</a>' +
+                  '<a href="javascript:;" class="dropdown-item">' +
+                  feather.icons['archive'].toSvg({ class: 'me-50 font-small-4' }) +
+                  'Archive</a>' +
+                  '<a href="javascript:;" class="dropdown-item delete-record">' +
+                  feather.icons['trash-2'].toSvg({ class: 'me-50 font-small-4' }) +
+                  'Delete</a>' +
+                  '</div>' +
+                  '</div>' +
+                  '<a href="javascript:;" class="item-edit">' +
+                  feather.icons['edit'].toSvg({ class: 'font-small-4' }) +
+                  '</a>'
+                );
+              }
+            }
+          ],
+          language: {
+            url: "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json",
+          },
+    
+      });
+  
+    $("div.head-label").html('<h6 class="mb-0">DataTable with Buttons</h6>');
+}
+});
